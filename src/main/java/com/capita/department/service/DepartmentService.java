6d1dab09ade64f28176177085c36a26450462fa1
pradipchoudhari <pradip.choudhari937@gmail.com>
2022-01-22 20:53:10 +0530

    added Logger variable in DepartmentService

--- src/main/java/com/capita/department/service/DepartmentService.java
+++ src/main/java/com/capita/department/service/DepartmentService.java
@@ -2,15 +2,22 @@ package com.capita.department.service;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.capita.department.controller.DepartmentController;
 import com.capita.department.entity.Department;
 import com.capita.department.repo.DepartmentRepo;
 
 @Service
 public class DepartmentService {
 
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(DepartmentService.class);
+
+	
 	@Autowired
 	private DepartmentRepo departmentRepo;
 	
